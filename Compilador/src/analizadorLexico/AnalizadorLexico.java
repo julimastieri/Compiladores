87fbd4cfb1c93487ea71b7b2d63244a2646b4a75
Maria Pia <maripigoico@gmail.com>
2019-09-26 16:33:08 -0300

    tds

--- Compilador/src/analizadorLexico/AnalizadorLexico.java
+++ Compilador/src/analizadorLexico/AnalizadorLexico.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import AnalizadorLexico.AnalizadorLexico;
+import AnalizadorLexico.Token;
+
 
 public class AnalizadorLexico {
 	
@@ -28,12 +31,29 @@ public class AnalizadorLexico {
     static final String TIPO_CTE_ULONG = "constante ulong";
     static final String TIPO_CADENA = "cadena";
     static final String TIPO_OPERADOR = "operador";
+    static final String TIPO_COMPARADOR = "comparador";
+    static final String TIPO_ASIGNACION = "asignacion";
+    static final String TIPO_PALABRA_RESERVADA = "palabra reservada";
+
     
     static final int ID = 257;
     static final int CTE = 258;
     static final int CADENA = 259;
     static final int ASIGNACION = 260;
-    
+    static final int MENOR_IGUAL = 261;
+    static final int MAYOR_IGUAL = 262;
+    static final int IGUAL_IGUAL = 263;
+    static final int DISTINTO = 264;
+    static final int IF = 265;
+    static final int ELSE = 266;
+    static final int END_IF = 267;
+    static final int PRINT = 268;
+    static final int INT = 269;
+    static final int BEGIN = 270;
+    static final int END = 271;
+    static final int FOREACH = 272;
+    static final int IN = 273;
+    static final int ULONG = 274;
     
     public AnalizadorLexico(File file) throws FileNotFoundException {
     	
@@ -49,7 +69,54 @@ public class AnalizadorLexico {
     }
     
     private void llenarTablaDeSimbolos() {
+
+            //Asignacion
+            Token token = new Token(":=",TIPO_ASIGNACION, ASIGNACION);
+            tablaSimbolos.put(":=", token);
+            
+            //Comparadores
+            token = new Token("<=",TIPO_COMPARADOR, MENOR_IGUAL);
+            tablaSimbolos.put("<=", token);
+            
+            token = new Token(">=",TIPO_COMPARADOR, MAYOR_IGUAL);
+            tablaSimbolos.put(">=", token);
+            
+            token = new Token("==",TIPO_COMPARADOR, IGUAL_IGUAL);
+            tablaSimbolos.put("==", token);
+            
+            token = new Token("<>",TIPO_COMPARADOR, DISTINTO);
+            tablaSimbolos.put("<>", token);
+        
+            //Palabras reservadas
+            token = new Token("if",TIPO_PALABRA_RESERVADA, IF);
+            tablaSimbolos.put("if", token);
+            
+            token = new Token("else",TIPO_PALABRA_RESERVADA, ELSE);
+            tablaSimbolos.put("else", token);
+            
+            token = new Token("end_if",TIPO_PALABRA_RESERVADA, END_IF);
+            tablaSimbolos.put("end_if", token);
+            
+            token = new Token("print",TIPO_PALABRA_RESERVADA, PRINT);
+            tablaSimbolos.put("print", token);
+            
+            token = new Token("int",TIPO_PALABRA_RESERVADA, INT);
+            tablaSimbolos.put("int", token);
+            
+            token = new Token("begin",TIPO_PALABRA_RESERVADA, BEGIN);
+            tablaSimbolos.put("begin", token);
     	
+            token = new Token("end",TIPO_PALABRA_RESERVADA, END);
+            tablaSimbolos.put("end", token);
+            
+            token = new Token("foreach",TIPO_PALABRA_RESERVADA, FOREACH);
+            tablaSimbolos.put("foreach", token);
+            
+            token = new Token("in",TIPO_PALABRA_RESERVADA, IN);
+            tablaSimbolos.put("in", token);
+            
+            token = new Token("ulong",TIPO_PALABRA_RESERVADA, ULONG);
+            tablaSimbolos.put("ulong", token);
     }
     
     public Token getNextToken() throws IOException {
